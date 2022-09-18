126b34e757b89dffe44a49a5663a9f5565c9f740
Cem Eliguzel <ce@concordium.com>
2022-09-18 11:21:42 +0300

    Displays UI with hello world

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use bracket_terminal::prelude::{main_loop, BError, BTermBuilder, GameState};
+
+struct State;
+
+impl GameState for State {
+    fn tick(&mut self, ctx: &mut bracket_terminal::prelude::BTerm) {
+        ctx.print_centered(25, "Hello, world!");
+    }
+}
+
+fn main() -> BError {
+    let context = BTermBuilder::simple80x50()
+        .with_title("Rusty Snake")
+        .build()?;
+    let gs = State;
+    main_loop(context, gs)
 }
